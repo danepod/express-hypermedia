f648478f6ac0bdca579dd50c139933fec0134e62
Dane Podzkiewitz <danepod@users.noreply.github.com>
2018-01-08 20:34:57 +0100

    Implement more robust content negotiation for errors

--- src/error.ts
+++ src/error.ts
@@ -18,31 +18,46 @@ export function errorMiddleware(err: RequestError, req: Request, res: Response, 
         message = err.message || "",
         error = req.app.get('env') === 'development' ? err.stack : {};
 
-    res.status(status);
-
-    const accepts = req.header('Accept') || "*/*";
-
-    switch (accepts) {
-        case "application/vnd.siren+json":
+    const errorHandlers: {
+        [method: string]: Function
+    } = {
+        "application/vnd.siren+json": () => {
             res.json(new Siren.ErrorProvider({
                 status,
                 message,
                 error
             }));
-            break;
-        case "application/vnd.collection+json":
+        },
+        "application/vnd.collection+json": () => {
             res.json(new CJ.StatusProvider({
                 title: message,
                 message: error
             }));
-            break;
-        default:
+        },
+        "default": () => {
             res.json({
                 status,
                 message,
                 error
             });
-            break;
+        }
+    };
+
+    res.status(status);
+
+
+    const acceptHeader = req.header('Accept');
+    const accepts = ((acceptHeader !== undefined) ? acceptHeader.replace(' ', '').split(',') : ['*/*']);
+
+    // Search the accept header for supported formats
+    const formatMatch = accepts.find(format => {
+        return errorHandlers[format] !== undefined;
+    });
+
+    if (formatMatch) {
+        errorHandlers[formatMatch]();
+    } else {
+        errorHandlers['default']();
     }
 
     next(err);
