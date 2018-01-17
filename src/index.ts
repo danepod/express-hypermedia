5c2ddcda707069a45cb28c0efe6abb0483514fac
Dane Podzkiewitz <danepod@users.noreply.github.com>
2018-01-17 19:44:25 +0100

    Add .travis.yml

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import { Resource } from './Resource';
-import { Representation, sortAcceptHeader } from './Representation';
+import { Representation } from './Representation';
 import * as Interfaces from './interfaces';
 import { RequestError, errorMiddleware } from "./error";
 
 import * as Siren from './adapters/siren';
 import * as CJ from './adapters/collectionjson';
-
 
 export { 
     Resource,
