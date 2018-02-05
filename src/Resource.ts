9a1ced0aaea0eb51dc0e71b625ba7f6c1ca564e8
Dane Podzkiewitz <danepod@users.noreply.github.com>
2018-02-05 16:48:41 +0100

    Add JSDoc for Resource and ResouceIdentifier

--- src/Resource.ts
+++ src/Resource.ts
@@ -4,11 +4,32 @@ import { Router } from "express";
 import { ResourceIdentifier } from "./ResourceIdentifier";
 
 // Resource Implementation ----------------------------------------------------
+/**
+ * The Resource class is used to describe a Resource as it is defined in [Roy Fieldings Thesis](https://www.ics.uci.edu/~fielding/pubs/dissertation/rest_arch_style.htm#sec_5_2_1_1).
+ * 
+ * Use a Resource to describe a certain thing or entity, for example ['Movies'](https://github.com/danepod/movie-database/blob/master/src/routes/movies.ts). You can assign a base url to a Resource and then add ResourceIdentifiers, which describe mappings to entities of a Resource (e.g. a list of movies or one movie in particular).
+ */
 export class Resource {
+    /**
+     * A canonical identifier for a Resource, e.g. `movie`
+     */
     name: string;
+
+    /**
+     * A canonical base URL for a Resource, e.g. `/movies`
+     */
     url: string; // TODO: Introduce URL type to enforce valid (relative) URLs
+
+    /**
+     * Array containing all ResourceIdentifiers of a Resource
+     */
     resourceIdentifiers: ResourceIdentifier[] = [];
 
+    /**
+     * Create a new Resource
+     * @param name Canonical identifier of the Resource
+     * @param url Canonical base URL of the Resource. If omitted, the URL will be set to `/name+s`
+     */
     constructor(name: string, url?: string) {
         url = url || `/${name}s`;
 
@@ -20,12 +41,19 @@ export class Resource {
         this.url = url;
     }
 
+    /**
+     * Add ResourceIdentifiers to the Resource
+     * @param resourceIdentifiers ResourceIdentifiers to be added to the Resource. Can be a single ResourceIdentifier or an array of ResourceIdentifiers
+     */
     addIdentifiers(resourceIdentifiers: ResourceIdentifier | ResourceIdentifier[]) {
         const addedIdentifiers: ResourceIdentifier[] = Array.isArray(resourceIdentifiers) ? resourceIdentifiers : [resourceIdentifiers];
 
         this.resourceIdentifiers.push(...addedIdentifiers);
     }
 
+    /**
+     * Generate an express Router from this Resource
+     */
     getExpressRouter(): Router {
         const router = Router();
 
@@ -36,7 +64,6 @@ export class Resource {
             for (let method in handlers) {
                 let handler = handlers[method];
 
-                // TODO: Add more HTTP verbs
                 switch (method.toUpperCase()) {
                     case 'GET':
                         router.get(`${this.url}${url}`, handler);
--- src/ResourceIdentifier.ts
+++ src/ResourceIdentifier.ts
@@ -3,16 +3,39 @@ import { Request, Response, NextFunction, Handler } from "express";
 import { RequestError } from "./error";
 import { sortAcceptHeader } from "./helpers";
 
-// ResourceIdentifier implementation ----------------------------------------------
+// ResourceIdentifier implementation ------------------------------------------
+/**
+ * A ResourceIdentifier is used to describe the mapping of a Resource to the entities covered by it.
+ * 
+ * Use one ResourceIdentifier for each mapping you want to create to an entity. For example, [a list of all movies](https://github.com/danepod/movie-database/blob/master/src/resources/movie/list/index.ts) and [one particular movie](https://github.com/danepod/movie-database/blob/master/src/resources/movie/detail/index.ts).
+ */
 export class ResourceIdentifier {
+    /**
+     * An array of keywords, describing the ResourceIdentifier. Useful for some hypermedia formats which use a self-describing string or array of strings for self-description, e.g. the `class` property of Siren.
+     */
     keywords: string[] = [];
+
+    /**
+     * The sub-URL of this ResourceIdentifier. When generating an express Router from a Resource, the sub-URL will be appended to the base URL of the Resource.
+     * 
+     * For example: `/movies` is the base URL defined in the Resource, `/:id` is the defined sub-URL in the ResourceIdentifier. The resulting URL for the Router will be `/movies/:id`.
+     */
     url: string;
+
+    /**
+     * Object containing all route handlers of a ResourceIdentifier instance.
+     */
     handlers: {
         [method: string]: {
             [format: string]: Handler
         }
     } = {};
 
+    /**
+     * 
+     * @param url Sub-URL of this ResouceIdentifier, supports express route parameters (e.g. `/:id`. This gets appended to the Resources base URL to form the full URL of the resulting express Router.
+     * @param keywords Keywords describing the nature of this ResourceIdentifiers entitiy. Can be a single string or an array of strings. Optional.
+     */
     constructor(url: string, keywords?: string | string[]) {
         this.url = url;
 
@@ -22,6 +45,20 @@ export class ResourceIdentifier {
         }
     }
 
+    /**
+     * Add route handlers to this ResourceIdentifier.
+     * 
+     * Call this method for every format you want to deliver.
+     * @param format MIME type of the format to be used, e.g. `application/vnd.siren+json`. Note that each adapter exports this through the `mime` property so you may use `Siren.mime` instead.
+     * @param handlers Object containing all route handlers for each HTTP method for the above defined MIME tyoe. Example `{ "GET": (req, res, next) => res.json(...), "POST": ... }`
+     * @param fallback Set this to `true`, if you want this format to act as a fallback in case an HTTP client accepts no format defined in this ResourceIdentifier but states it could handle any format. 
+     * 
+     * If no format has set this to `true`, each request that doesn't accept any of the defined formats will yield an HTTP 406 (Not Acceptable) error. 
+     * 
+     * Setting multiple formats to be the fallback format will cause the last evaluated format to act as the fallback. 
+     * 
+     * Optional, default `false`.
+     */
     addHandlers(
         format: string,
         handlers: { [method: string]: Handler },
@@ -39,6 +76,11 @@ export class ResourceIdentifier {
         }
     }
 
+    /**
+     * This method bundles all format handlers for every HTTP verb and format by creating a route handler that performs content negotiation.
+     * 
+     * Usually, you do not need to call this method in an application that uses this library as it gets called when using getExpressRouter() from a Resource instance.
+     */
     getRoute(): {
         [method: string]: Handler
     } {
