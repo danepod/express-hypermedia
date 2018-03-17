e9e825186bd9ba98202583c9a9fb4f9d264a70f2
Dane Podzkiewitz <danepod@users.noreply.github.com>
2018-03-17 16:51:33 +0100

    Export Entity, Provider and Action base classes
    This enables external adapters to use these classes

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import { ResourceIdentifier } from './ResourceIdentifier';
 import * as Interfaces from './interfaces';
 import { RequestError, errorMiddleware, errorHandlerFunction } from "./error";
 
+import { Entity } from "./Entity";
+import { Provider } from "./Provider";
+import { Action } from "./Action";
+
 import * as Siren from './adapters/siren';
 import * as CJ from './adapters/collectionjson';
 
@@ -14,5 +18,8 @@ export {
     CJ,
     RequestError,
     errorMiddleware,
-    errorHandlerFunction
+    errorHandlerFunction,
+    Entity,
+    Provider,
+    Action
 };
